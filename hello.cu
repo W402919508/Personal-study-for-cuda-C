3f19cee6d496a511b83fe94875cab0812feda490
W402919508 <1354470106@qq.com>
2026-03-11 17:12:22 +0800

    100 to 1000

--- hello.cu
+++ hello.cu
@@ -19,7 +19,7 @@ int deviceCount;
         printf("Device %d: %s\n", dev, deviceProp.name);
     }
     cudaSetDevice(0);
-    int nElm = 100;
+    int nElm = 1000;
     dim3 blockSize (nElm/5);
     dim3 gridSize ((nElm+blockSize.x-1)/blockSize.x);
     //blockSize.x = nElm/5;
